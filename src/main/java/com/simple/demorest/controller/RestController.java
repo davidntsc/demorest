c59a2c610dd52e40fe47c34f679fb8a7a56b2dba
Nitescu <david.c.nitescu@accenture.com>
2020-06-10 13:31:11 +0300

    first commit

---
+++ src/main/java/com/simple/demorest/controller/RestController.java
@@ -0,0 +1,13 @@
+package com.simple.demorest.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "demo";
+    }
+}
